cde6be9eb58b99701a55f32297d636425bd1656b
Campalans <101204872+Campalans03@users.noreply.github.com>
2025-10-13 13:10:56 +0200

    eslint fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,52 +2,49 @@ import './App.css';
 import { useAllVideos } from './useAllVideos';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src="/protube-logo-removebg-preview.png" className="App-logo" alt="logo" />
-                <ContentApp />
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/protube-logo-removebg-preview.png" className="App-logo" alt="logo" />
+        <ContentApp />
+      </header>
+    </div>
+  );
 }
 
 function ContentApp() {
-    const { loading, message, value } = useAllVideos();
+  const { loading, message, value } = useAllVideos();
 
-    switch (loading) {
-        case 'loading':
-            return <div>Loading...</div>;
+  switch (loading) {
+    case 'loading':
+      return <div>Loading...</div>;
 
-        case 'error':
-            return (
-                <div>
-                    <h3>Error</h3>
-                    <p>{message}</p>
-                </div>
-            );
+    case 'error':
+      return (
+        <div>
+          <h3>Error</h3>
+          <p>{message}</p>
+        </div>
+      );
 
-        case 'success':
-            // convertir el JSON en un array de valores
-            const myarray = Object.values(value || {});
+    case 'success':
+      // convertir el JSON en un array de valores
+      //const myarray = Object.values(value || {});
+      return (
+        <>
+          <strong>Videos available:</strong>
+          <ul>
+            {[].map((item, index) => (
+              <li key={index}>
+                {item.id ? item.id : JSON.stringify(item)} {/* por si no hay "title", mostrar algo */}
+              </li>
+            ))}
+          </ul>
+        </>
+      );
+  }
 
-            return (
-                <>
-                    <strong>Videos available:</strong>
-                    <ul>
-                        {myarray.map((item, index) => (
-                            <li key={index}>
-                                {item.id
-                                    ? item.id
-                                    : JSON.stringify(item)} {/* por si no hay "title", mostrar algo */}
-                            </li>
-                        ))}
-                    </ul>
-                </>
-            );
-    }
-
-    return <div>Idle...</div>;
+  return <div>Idle...</div>;
 }
 
 export default App;
