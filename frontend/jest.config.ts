565989fc7af1003370a34de5e183238cdc4534ba
Campalans <101204872+Campalans03@users.noreply.github.com>
2025-11-10 11:26:49 +0100

    jest.config

---
+++ frontend/jest.config.ts
@@ -0,0 +1,23 @@
+import { createDefaultPreset } from 'ts-jest';
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+export default {
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  collectCoverage: true,
+  coverageReporters: ['json', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 1,
+      functions: 1,
+      lines: 1,
+      statements: 1,
+    },
+  },
+};
